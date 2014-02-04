b24dd4206acc49f8593f9463fc2a31d71ea4f444
mocamoca3 <monicalaskos@gmail.com>
2014-02-03 19:03:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleClone::Application.config.secret_key_base = 'dc6032f92b56b63b429abfdc7b088a5222d23122d2fc4392c51cbf156713868de3937775898eea1cd2d90d88fa1fd9ae6fd62cefa4e883ec2b276ec98560bea3'
